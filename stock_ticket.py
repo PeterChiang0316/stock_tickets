3f4dd955d830eb12dfbcfd4ef24ac8cd01d39790
wty <bn51401@hotmail.com>
2018-06-10 01:12:17 +0800

    Add 0607, 0608 data and trail phase in features

--- stock_ticket.py
+++ stock_ticket.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import time, datetime
+import time, datetime, collections
 from tools.stock_price_utility import Stock
 from multiprocessing import Pool, Lock
 
@@ -19,33 +19,43 @@ def magnitude_test(stock):
     #print 'percentage: %.2f' % (sum(i > 0.02 for i in magnitude) / float(len(magnitude)))
     return (sum(i > 0.02 for i in magnitude) / float(len(magnitude)))
     
-def stock_win_point_test(stock):
+def stock_win_point_test(stock, minutes=5):
+
+    def tick_after_minutes(time, minutes):
+        now = datetime.datetime(2018, 5, 25, time/10000, (time/100) % 100, time % 100)
+        after = now + datetime.timedelta(minutes=minutes)
+        return after
+    
     ship = Stock(stock)
     
     info = ship.get_daily_info('20180525', every_transaction=True)
     details = info.data
     
-    now = datetime.datetime(2018, 5, 25, 9, 0, 34)
-    print now
     
-    for tick, data in details.items()[:5]:
-        print datetime.datetime(2018, 5, 25, int(tick[:2]), int(tick[2:4]), int(tick[4:])) 
-        print datetime.datetime(2018, 5, 25, int(tick[:2]), int(tick[2:4]), int(tick[4:])) + datetime.timedelta(minutes=5)
+    trace = collections.OrderedDict()
+    
+    for k, v in details.items():
+        trace[int(k)] = v
+    
+    
+    for tick, data in trace.items()[:5]:
+        print tick
+        print tick_after_minutes(tick, minutes)
+        #print tick, data.deal, data.buy, data.sell, data.count, data.diff
         
-        print tick, data.deal, data.buy, data.sell, data.count, data.diff
     
 if __name__ == '__main__':
 
     
-    #stock_win_point_test('2454')    
+    stock_win_point_test('2454')    
     
     stock_list = ['2454', '2439', '2330', '2455', '2448', '2377', '3035',\
                   '2456', '2313', '5269', '2383', '1312', '2353', '1707',\
                   '3443', '4906']
                   
-    for s in stock_list:
-        ship = Stock(s)
-        ship.update_daily_info('20180605')
+    #for s in stock_list:
+    #    ship = Stock(s)
+    #    ship.update_daily_info('20180608')
 
 
             
