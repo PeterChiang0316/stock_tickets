f439ed455b19cd6159ca16bf3b873c1e5a1fb8cc
wty <bn51401@hotmail.com>
2018-06-17 01:03:31 +0800

    daft version

--- stock_ticket.py
+++ stock_ticket.py
@@ -2,6 +2,7 @@
 import time, datetime, collections
 from tools.stock_price_utility import Stock
 from multiprocessing import Pool, Lock
+import argparse
 
 def magnitude_test(stock):
     
@@ -24,38 +25,101 @@ def stock_win_point_test(stock, minutes=5):
     def tick_after_minutes(time, minutes):
         now = datetime.datetime(2018, 5, 25, time/10000, (time/100) % 100, time % 100)
         after = now + datetime.timedelta(minutes=minutes)
-        return after
-    
+        return int("%02d%02d%02d" % (after.hour, after.minute, after.second))
+        
+    def get_element_list(l, attr):
+        return [e[attr] for e in l]
+        
+    def time_diff(t1, t2):
+        t1 = datetime.datetime(2018, 5, 25, t1/10000, (t1/100) % 100, t1 % 100)
+        t2 = datetime.datetime(2018, 5, 25, t2/10000, (t2/100) % 100, t2 % 100)
+        return (t2 - t1).total_seconds()
+        
     ship = Stock(stock)
     
     info = ship.get_daily_info('20180525', every_transaction=True)
     details = info.data
-    
     
     trace = collections.OrderedDict()
     
     for k, v in details.items():
         trace[int(k)] = v
     
+    tick_begin = trace.keys()[0]
     
-    for tick, data in trace.items()[:5]:
-        print tick
-        print tick_after_minutes(tick, minutes)
-        #print tick, data.deal, data.buy, data.sell, data.count, data.diff
+    for tick, data in trace.items():
+        
+        if tick < tick_begin:
+            continue
+            
+        price = data.deal
+        target_price = price * 1.015
+
+        next_tick = tick_after_minutes(tick, minutes)
+        ticks = collections.OrderedDict()
+        
+        for interval_tick, interval_data in trace.items():
+            if tick <= interval_tick and interval_tick <= next_tick:
+                ticks[interval_tick] = interval_data
+
+        max_value = max(get_element_list(ticks.values(), 'deal'))
+        if max_value > target_price:
+            tick_begin = next_tick
+        else:
+            continue
+        
+        #print ticks.items()
+        
+        buy_price = max_value * 0.99
+        
+        high_number, low_number = 0, 0
+        
+        for interval_tick, interval_data in ticks.items():
+        
+            if interval_data.deal >= interval_data.sell:
+                high_number += interval_data.count
+            else:
+                low_number += interval_data.count
+                
+            if interval_data.deal >= buy_price:
+                break
+                
+        print time_diff(tick, interval_tick), high_number, low_number, (high_number / (high_number+low_number)), price, max_value, buy_price, interval_data.deal 
+        
+        #print tick, , data.buy, data.sell, data.count, data.diff
         
     
 if __name__ == '__main__':
-
-    
-    stock_win_point_test('2454')    
-    
+    '''
+    Algorithm:
+        1. Scan through the next N minute, find the max deal price
+        2. Get the tick match the max deal price and check if it rise up over the threshold
+            if no: continue
+            else:
+                Find the earliest tick that we can buy and earn 1%
+                Calculate the accumalated number since the initial tick
+                Then we can know the 
+    '''   
     stock_list = ['2454', '2439', '2330', '2455', '2448', '2377', '3035',\
                   '2456', '2313', '5269', '2383', '1312', '2353', '1707',\
                   '3443', '4906']
-                  
-    #for s in stock_list:
-    #    ship = Stock(s)
-    #    ship.update_daily_info('20180608')
-
-
+    
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--update', help='Get today\'s data', action='store_true')    
+    args = parser.parse_args()
+    
+    if args.update:
+        for s in stock_list:
+            ship = Stock(s)
+            ship.update_daily_info('20180615')
+        exit(0)
+    
+    for s in stock_list[:]:   
+        print s
+        stock_win_point_test(s, minutes=5)  
+    
+    
+    
+    
+    
             
