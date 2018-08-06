4552faa5dd259adee7785a4d25130dcbe000d776
wty <bn51401@hotmail.com>
2018-08-07 01:07:02 +0800

    Fix typo

--- stock_analysis.py
+++ stock_analysis.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import seaborn as sns
-import matplotlib
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('train_data_last5.csv')
+df = pd.read_csv('train_data.csv')
 
 df['win_ratio'] = df.win/(df.win+df.lose)
 df['buy_speed'] = df.number/df.second
-df['test'] = (df.main_ratio*df.inout_ratio)**0.5
-print df
 
-sns.lmplot(x="test", y="win_ratio", hue='inout_ratio', data=df)
+sns.relplot(x="number", y="win_ratio", hue='inout_ratio', data=df)
 
-matplotlib.pyplot.show()
+plt.xlim(0, None)
+plt.ylim(0, None)
+
+plt.show()
