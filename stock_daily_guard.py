642c72f654c1ad852e604726234614feff0ef1cb
wty <bn51401@hotmail.com>
2018-08-24 09:00:44 +0800

    Add 0824 stock data

--- stock_daily_guard.py
+++ stock_daily_guard.py
@@ -2,6 +2,7 @@ import stock_ticket
 from tools.stock_price_utility import Stock
 import pandas as pd
 import bisect
+import numpy as np
 
 if __name__ == '__main__':
 
@@ -10,9 +11,9 @@ if __name__ == '__main__':
     
     win_standard_dict = {}
     sim_start_date = '20180525'
-    sim_end_date = '20180715'
+    sim_end_date = '20180815'
     
-    df = pd.DataFrame(columns=('s', 'date', 'last_date', 'K9', 'D9', 'RSI10', 'RSI5', 'DIF_MACD', 'HIT'))
+    df = pd.DataFrame(columns=('s', 'date', 'last_date', 'K9', 'D9', 'RSI10', 'RSI5', 'DIF_MACD', 'STD5', 'STD10', 'HIT'))
     finance = ship.get_stock_finance()
     
     
@@ -33,11 +34,19 @@ if __name__ == '__main__':
         left = bisect.bisect_left(finance.keys(), date)
         last_date = finance.keys()[left-1]
         last_finance = finance.values()[left-1]
+        print last_finance
+
+        last5 = map(lambda v:v['ClosePr'], finance.values()[left-6:left-1])
+        std5 = np.std(last5, ddof=1)
+
+        last10 = map(lambda v: v['ClosePr'], finance.values()[left - 11:left - 1])
+        std10 = np.std(last10, ddof=1)
 
         df = df.append(pd.Series({'s':s, 'date':date, 'last_date': last_date,                         \
                                         'K9':last_finance['K9'], 'D9':last_finance['D9'],             \
                                         'RSI10':last_finance['RSI10'], 'RSI5':last_finance['RSI5'],   \
                                         'DIF_MACD':last_finance['DIF_MACD'],
+                                        'STD5':std5, 'STD10':std10,
                                         'HIT':len(win_standards)}), ignore_index=True)
     
     df.to_csv('2456_report.csv')
